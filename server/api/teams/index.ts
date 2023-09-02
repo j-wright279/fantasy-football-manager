173b9e5cf5354d4d5685b789f970c2680055917f
Jamie <100721920+Jiblets4Free@users.noreply.github.com>
2023-09-02 18:41:58 +0100

    fix: removing unused teams api

--- server/api/teams/index.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const data: any = await $fetch("https://fantasy.premierleague.com/api/bootstrap-static/", {
-        method: "GET"
-    }).catch((err) => { teams: []})
-    return { teams: data.teams }
-})
